0e879411360e65610424c9e02c564bc105a10507
dnluc <dlucchelli@gmail.com>
2021-07-02 20:27:36 -0300

    Update RecorreArbol.java

--- src/ar/caece/algoritmos2/arbol/RecorreArbol.java
+++ src/ar/caece/algoritmos2/arbol/RecorreArbol.java
@@ -10,44 +10,26 @@ public class RecorreArbol<T> {
 	@SuppressWarnings("unchecked")
 	public List<T> recorrer(ArbolBajador arbol, int salto) {
 
-//		switch(arbol.tipoNodo) {
-//		case VALOR:
-//			if (salto == 0) {
-//				recorrido.add(((ArbolBajadorValor<T>) arbol).getValor());
-//			}
-//			break;
-//		case SALTEADOR:
-//			salto = ((ArbolBajadorSalteador) arbol).getValor(); 			
-//			break;
-//		case MODIFICADOR:
-//			if (salto > 0) {
-//				salto = salto + ((ArbolBajadorModificador) arbol).getValor();
-//				if (salto < 0) {
-//					salto = 0;
-//				}
-//			}
-//			break;
-//		default:
-//			break;
-//		}
-
-		
-		if (arbol instanceof ArbolBajadorValor) {
+		switch(arbol.tipoNodo) {
+		case VALOR:
 			if (salto == 0) {
 				recorrido.add(((ArbolBajadorValor<T>) arbol).getValor());
 			}
-		} else if (arbol instanceof ArbolBajadorSalteador) {
-			salto = ((ArbolBajadorSalteador) arbol).getValor();
-			
-		} else if (arbol instanceof ArbolBajadorModificador && salto > 0) {
+			break;
+		case SALTEADOR:
+			salto = ((ArbolBajadorSalteador) arbol).getValor(); 			
+			break;
+		case MODIFICADOR:
+			if (salto > 0) {
 				salto = salto + ((ArbolBajadorModificador) arbol).getValor();
 				if (salto < 0) {
 					salto = 0;
 				}
+			}
+			break;
+		default:
+			break;
 		}
-		
-		
-		
 		
 		if (salto > 0)
 		{
